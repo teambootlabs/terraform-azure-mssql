548040bd7e62fc0dbd6c96ed65a982ebf3adc55a
viswath165 <vichumahesh@gamil.com>
2022-04-08 10:44:21 +0530

    (fix)terraform fmt

--- module.tf
+++ module.tf
@@ -1,9 +1,9 @@
-module "mssql"{
-    source = "./enviroment"
-    location = var.location
-    charset = var.charset
-    collation = var.collation
-    mssql_server_administraxtion_login =var.mssql_server_administraxtion_login
-    mssql_server_administraxtion_login_password =var.mssql_server_administraxtion_login_password
-    name = var.name
+module "mssql" {
+  source                                      = "./enviroment"
+  location                                    = var.location
+  charset                                     = var.charset
+  collation                                   = var.collation
+  mssql_server_administraxtion_login          = var.mssql_server_administraxtion_login
+  mssql_server_administraxtion_login_password = var.mssql_server_administraxtion_login_password
+  name                                        = var.name
 }
